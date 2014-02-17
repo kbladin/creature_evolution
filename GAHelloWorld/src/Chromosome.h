3fa3ea9e41d834fdd0f3ef489ce933e51e8eb14a
fredlb <fredrik.lindner@gmail.com>
2014-02-17 11:55:57 +0100

    fixed c++11 support in cmake

--- GAHelloWorld/src/Chromosome.h
+++ GAHelloWorld/src/Chromosome.h
@@ -35,7 +35,7 @@ public:
 
 struct ChromosomeLessThan
 {
-	bool operator()( Chromosome& c1,  Chromosome& c2) {
+	bool operator()(const Chromosome& c1,const Chromosome& c2) {
 		return c1.GetFitness() < c2.GetFitness();
 	}
 };
