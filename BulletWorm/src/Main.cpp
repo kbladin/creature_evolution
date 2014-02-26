851612c190e7bfb55711ab2ddaed3ef647bfad82
kbladin <kallebladin@gmail.com>
2014-02-26 20:23:49 +0100

    Changed from glut to glfw. No render function that supports OpenGL 3.2 yet.

--- BulletWorm/src/Main.cpp
+++ BulletWorm/src/Main.cpp
@@ -3,7 +3,9 @@
 #include <stdlib.h>
 
 #include <GL/glew.h>
-#include <GL/freeglut.h>
+//#define GLFW_INCLUDE_GLCOREARB
+#include <GL/glfw3.h>
+//#include <GL/freeglut.h>
 
 #include "Simulation.h"
 #include "Renderer.h"
@@ -20,7 +22,36 @@ int current_time;
 Simulation* helloWorld;
 Renderer* bajs;
 
+GLFWwindow* window;
+int width, height;
+
+
 int main (int argc, char** argv) {
+
+    //glfwSetErrorCallback(error_callback);
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
+
+    // We want the newest version of OpenGL
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+
+    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+    if (!window){
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+    glfwMakeContextCurrent(window);
+    //glfwSetKeyCallback(window, key_callback);
+
+
+
+
+
+	/*
 	current_time = glutGet(GLUT_ELAPSED_TIME);
 	glutInitWindowSize(kWindowWidth, kWindowHeight);
 	glutInit(&argc, argv);
@@ -28,9 +59,10 @@ int main (int argc, char** argv) {
 	glutCreateWindow("Bullet Test");
 	glutDisplayFunc(render);
 	glutIdleFunc(update);
+	*/
 	// start GLEW extension handler
-	glewExperimental = GL_TRUE;
-	glewInit();
+	//glewExperimental = GL_TRUE;
+	//glewInit();
 
 	// get version info
 	const GLubyte* renderer = glGetString (GL_RENDERER); // get renderer string
@@ -44,7 +76,23 @@ int main (int argc, char** argv) {
 	//initialize debugDrawer for simulation
 	bajs = new Renderer(helloWorld);
 
-	glutMainLoop();
+
+
+
+    while (!glfwWindowShouldClose(window)){
+        glfwGetFramebufferSize(window, &width, &height);
+        // DRAW
+        update();
+
+        render();
+
+        //Swap draw buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+
+	//glutMainLoop();
 
 	return 0;
 }
@@ -61,8 +109,10 @@ void render()
 	bajs->render();
 
 	//must have
+	/*
 	glutSwapBuffers();
 	glutPostRedisplay();
+	*/
 }
 
 void update()
