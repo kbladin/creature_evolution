8d0a79fb9fdcd276af6a6a92953fb6b17f9e7e8a
fred <fredrik.lindner@gmail.com>
2014-03-24 22:16:52 +0100

    plane class can now take scale as input argument in constructor

--- src/Plane.cpp
+++ src/Plane.cpp
@@ -10,3 +10,21 @@ Plane::Plane() {
 	vertex_data_.push_back(glm::vec3(-1.0f,0.0f,-1.0f));
 
 }
+
+Plane::Plane(glm::vec3 scale) {
+	vertex_data_.push_back(glm::vec3(-1.0f,0.0f,-1.0f));
+	vertex_data_.push_back(glm::vec3(1.0f,0.0f,-1.0f));
+	vertex_data_.push_back(glm::vec3(1.0f,0.0f,1.0f));
+
+	vertex_data_.push_back(glm::vec3(1.0f,0.0f,1.0f));
+	vertex_data_.push_back(glm::vec3(-1.0f,0.0f,1.0f));
+	vertex_data_.push_back(glm::vec3(-1.0f,0.0f,-1.0f));
+
+	for(int i = 0; i < vertex_data_.size(); ++i) {
+		vertex_data_[i].x = vertex_data_[i].x*scale.x;
+		vertex_data_[i].y = vertex_data_[i].y*scale.y;
+		vertex_data_[i].z = vertex_data_[i].z*scale.z;
+		
+	}
+
+}
