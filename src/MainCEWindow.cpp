47435b116035476456b77d88958c0bf6485905fa
Gabriel Baravdish <gabba873@student.liu.se>
2014-04-14 10:37:32 +0200

    Added sliders and normalized parameters

--- src/MainCEWindow.cpp
+++ src/MainCEWindow.cpp
@@ -1,15 +1,14 @@
-
 #include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QSlider>
-#include <QtWidgets/QStackedWidget>
-
+#include <QtDebug>
+#include <QSignalMapper>
 #include "GLWidget.h"
 #include "MainCEWindow.h"
+#include "SettingsManager.h"
 
 MainCEWindow::MainCEWindow(CreatureEvolution* ce)
 {
-    std::cout << "Setting up window." << std::endl;
     creature_evo_ = ce;
     QGLFormat glFormat;
     glFormat.setVersion( 3, 2 );
@@ -18,40 +17,122 @@ MainCEWindow::MainCEWindow(CreatureEvolution* ce)
     //QGLFormat::setDefaultFormat(glFormat);
     glWidget = new GLWidget(glFormat,0,creature_evo_);
 
+    //QSignalMapper *signalMapper = new QSignalMapper(this);
+
+      /* for (int i = 0; i < 4; ++i) {
+        slide[i] = createSlider();
+        connect(slide[i], SIGNAL(valueChanged(int)), this, SLOT(setValue(int)));
+        controlLayout->addWidget(slide[i]);
+        signalMapper->setMapping(slide[i], i);
+        connect(slide[i], SIGNAL(valueChanged(int)), signalMapper, SLOT(map()));
+    }*/
+    //connect(signalMapper, SIGNAL(mapped(int)), this, SIGNAL(setValue(int)));
+
+    //connect(signalMapper, SIGNAL(mapped(int)), this, SIGNAL(digitClicked(int)));
+
+    /*SettingsManager::()->setPopulationSize(5);
+    SettingsManager::Instance()->setCrossover(0.8);
+    SettingsManager::Instance()->setElitism(0.2);
+    SettingsManager::Instance()->setMutation(0.8);
+    */
+
     QHBoxLayout *mainLayout = new QHBoxLayout;
-    QVBoxLayout* controlLayout = new QVBoxLayout;
+    QHBoxLayout *controlLayout = new QHBoxLayout;
+    QVBoxLayout *buttonLayout = new QVBoxLayout;
     QPushButton *dummyButton = new QPushButton("Push for pleasure!");
     QPushButton *simButton = new QPushButton("Start simulation.");
-    QPushButton *testButton = new QPushButton("Change Color");
+    QPushButton *testButton = new QPushButton("Change color");
 
-    controlLayout->addWidget(dummyButton);
-    controlLayout->addWidget(simButton);
-    controlLayout->addWidget(testButton);
+    QSignalMapper *signalMapper = new QSignalMapper(this);
 
+    slide[0] = createSlider(100, 1, 10, 10);
+    slide_pop = createSlider(100, 1, 10, 10);
+    slide_CO = createSlider(100, 1, 10, 10);
+    slide_elit = createSlider(100, 1, 10, 10);
+    slide_mut = createSlider(100, 1, 10, 10);
+
+    controlLayout->addWidget(slide[0]);
+    controlLayout->addWidget(slide_pop);
+    controlLayout->addWidget(slide_CO);
+    controlLayout->addWidget(slide_elit);
+    controlLayout->addWidget(slide_mut);
+
+    buttonLayout->addWidget(dummyButton);
+    buttonLayout->addWidget(simButton);
+    buttonLayout->addWidget(testButton);
     mainLayout->addWidget(glWidget);
     mainLayout->addLayout(controlLayout);
-
+    mainLayout->addLayout(buttonLayout);
     setLayout(mainLayout);
-
     setWindowTitle(tr("Creature Evolution"));
-    //connect(dummyButton,SIGNAL(clicked()), this, SLOT(testPrint()));
+
+    // ----- Connect buttons -----
     connect(dummyButton,SIGNAL(clicked()), glWidget, SLOT(enableRendering()));
     connect(simButton, SIGNAL(clicked()), this, SLOT(startEvolution()));
-    connect(testButton, SIGNAL(pressed()), this, SLOT(testChangeFun1()));
-    connect(testButton, SIGNAL(released()), this, SLOT(testChangeFun2()));
+    connect(testButton, SIGNAL(pressed()), this, SLOT(changePressed()));
+    connect(testButton, SIGNAL(released()), this, SLOT(changeReleased()));
+    // ----- Connect sliders -----
+    connect(slide[0], SIGNAL(valueChanged(int)), this, SLOT(setValueGen(int)));
+    connect(slide_pop, SIGNAL(valueChanged(int)), this, SLOT(setValuePop(int)));
+    connect(slide_CO, SIGNAL(valueChanged(int)), this, SLOT(setValueCO(int)));
+    connect(slide_elit, SIGNAL(valueChanged(int)), this, SLOT(setValueElit(int)));
 
-    std::cout << "Done!" << std::endl;
+
+    //createLayout();
 }
 
-QSlider *MainCEWindow::createSlider()
+QSlider *MainCEWindow::createSlider(int range, int step, int page, int tick)
 {
     QSlider *slider = new QSlider(Qt::Vertical);
-    slider->setRange(0, 360 * 16);
-    slider->setSingleStep(16);
-    slider->setPageStep(15 * 16);
-    slider->setTickInterval(15 * 16);
+    slider->setRange(0, range); // 10
+    slider->setSingleStep(step); // 1
+    slider->setPageStep(page); // 10
+    slider->setTickInterval(tick); // 10
     slider->setTickPosition(QSlider::TicksRight);
     return slider;
+}
+
+/*SettingsManager::()->setPopulationSize(5);
+SettingsManager::Instance()->setCrossover(0.8);
+SettingsManager::Instance()->setElitism(0.2);
+SettingsManager::Instance()->setMutation(0.8);*/
+void MainCEWindow::setValueGen(int value)
+{
+    SettingsManager::Instance()->setMaxGenerations(value);
+    qDebug()<<value;
+}
+
+void MainCEWindow::setValuePop(int value)
+{
+    SettingsManager::Instance()->setPopulationSize(value);
+    qDebug()<<value;
+}
+
+void MainCEWindow::setValueCO(int value)
+{
+    SettingsManager::Instance()->setCrossover(float(value/normalize));
+    qDebug()<<(float)value/normalize;
+}
+
+void MainCEWindow::setValueElit(int value)
+{
+    SettingsManager::Instance()->setElitism(float(value/normalize));
+    qDebug()<<(float)value/normalize;
+}
+void MainCEWindow::setValueMut(int value)
+{
+    SettingsManager::Instance()->setMutation(float(value/normalize));
+    qDebug()<<(float)value/normalize;
+}
+
+void MainCEWindow::changePressed()
+{
+
+}
+
+void MainCEWindow::changeReleased()
+{
+
 }
 
 void MainCEWindow::keyPressEvent(QKeyEvent *e)
@@ -73,12 +154,3 @@ void MainCEWindow::startEvolution() {
 void MainCEWindow::renderWorm() {
 
 }
-
-void MainCEWindow::testChangeFun1() {
-    std::cout << "Hello!" << std::endl;
-}
-
-void MainCEWindow::testChangeFun2() {
-    std::cout << "Bye!" << std::endl;
-}
-
--- src/include/MainCEWindow.h
+++ src/include/MainCEWindow.h
@@ -1,16 +1,12 @@
 #ifndef MAINCEWINDOW_H
 #define MAINCEWINDOW_H
 
+#include <QtWidgets/QWidget>
+#include <QtWidgets/QGroupBox>
 #include <CreatureEvolution.h>
 
 QT_BEGIN_NAMESPACE
 class QSlider;
-class QCheckBox;
-class QComboBox;
-class QGroupBox;
-class QLabel;
-class QSpinBox;
-class QStackedWidget;
 QT_END_NAMESPACE
 
 class GLWidget;
@@ -21,21 +17,35 @@ class MainCEWindow : public QWidget
 
 public:
     MainCEWindow(CreatureEvolution* ce);
+signals:
+    void valueChanged(int value);
 
 public slots:
     void testPrint();
     void startEvolution();
     void renderWorm();
-    void testChangeFun1();
-    void testChangeFun2();
+
+    void setValueGen(int value);
+    void setValuePop(int value);
+    void setValueCO(int value);
+    void setValueElit(int value);
+    void setValueMut(int value);
+    void changePressed();
+    void changeReleased();
 protected:
     void keyPressEvent(QKeyEvent *event);
-
 private:
-    QSlider *createSlider();
-
+    QSlider *createSlider(int range, int step, int page, int tick);
+    QSlider *slide[4];
+    QSlider *slide_gen;
+    QSlider *slide_pop;
+    QSlider *slide_CO;
+    QSlider *slide_elit;
+    QSlider *slide_mut;
     GLWidget *glWidget;
     CreatureEvolution* creature_evo_;
+    static const int normalize = 100;
+    int val;
 };
 
 #endif // MAINCEWINDOW_H
